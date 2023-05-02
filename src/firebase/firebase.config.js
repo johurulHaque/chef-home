c8253bae12f169e40dbed4a85db7216f09172e86
johurulhque <johurulhque.cse@gmail.com>
2023-05-02 09:19:02 +0600

    title and title image is set

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUgmAB74eSGwbaS0Cgu_9BXtPtLC0fSDo",
+  authDomain: "chef-home-bb09f.firebaseapp.com",
+  projectId: "chef-home-bb09f",
+  storageBucket: "chef-home-bb09f.appspot.com",
+  messagingSenderId: "895060833054",
+  appId: "1:895060833054:web:1ba38d57ccd49a74280f0f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
